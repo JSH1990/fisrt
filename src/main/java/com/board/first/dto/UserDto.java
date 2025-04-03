5e66eb5713ed8a1a64cac1012e0082eeeb20bdc7
jshblack <118979741+JSH1990@users.noreply.github.com>
2025-04-03 09:20:43 +0900

    LoginUser

--- src/main/java/com/board/first/dto/UserDto.java
+++ src/main/java/com/board/first/dto/UserDto.java
@@ -1,10 +1,21 @@
 package com.board.first.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 public class UserDto {
 
+    private Long id;
+    private String name;
+    private String email;
+    private String address;
+    private String role;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdDate;
 }
